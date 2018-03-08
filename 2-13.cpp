a5b995aedddadec33d1a071608e989c6645cb16c
madwayz <djurball@list.ru>
2018-03-08 12:08:19 +0800

    Add files via upload

--- 2-13.cpp
+++ 2-13.cpp
@@ -1,40 +1,21 @@
 #include <stdafx.h>
 #include <iostream>
 #include <cstdlib>
-#include <cmath>
+#include<ctime>
 using namespace std;
 
-long Reverse(long x)
-{
-	long y = 0;
-	while (x)
-	{
-		y = y * 10 + x % 10;
-		x /= 10;
-	}
-	return y;
-}
 
-int main()
+int main() 
 {
 	setlocale(0, "Russian");
-	int number = 0;
-	cout << "Введите трёхзначное число: " << endl;
-	cin >> number;
-	int count = log10(number) + 1;
-
-	while(count != 3)
-	{
-		cout << "[ОШИБКА!] Введите трёхзначное целое число: ";
-		cin >> number;
-		int count = log10(number) + 1;
-		if(count == 3)
-		{
-			cout << "Инверсное число: " << Reverse(number) << endl;
-			break;
-		}
-	}
-
+	srand(time(0));
+	int number = rand() % 999 + 100;
+	
+	cout << "Дано число: " << number << endl;
+	int units = number % 10;
+	int tens =  (number / 10) % 10;
+	int hundreds = number / 10 / 10;
+	cout << "Ответ: " << units << tens << hundreds << endl;
 	system("pause");
 	return 0;
 }
