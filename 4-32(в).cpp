ff793dbf1a9916984feab3a3e63284281968c539
madwayz <djurball@list.ru>
2018-03-08 12:18:32 +0800

    Add files via upload

--- 4-32(в).cpp
+++ 4-32(в).cpp
@@ -1,12 +1,13 @@
 #include "stdafx.h"
 #include <iostream>
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
+
 using namespace std;
 
 /*
 
-ЗАДАНИЕ 4.28 (в)
+ЗАДАНИЕ 4.32 (в)
 
 */
 
@@ -18,17 +19,14 @@ int main()
 	int num_1 = number / 1000;
 	int num_2 = (number / 100) % 10;
 	int num_3 = (number / 10) % 10;
-	int num_4 = number % 10;
-	int proizv = num_1 * num_2 * num_3 * num_4;
-	cout << "Дано число: " << number << endl;
-	if (proizv == 0) {
-		cout << "Произведение чисел равно 0 и не может быть вычислено" << endl;
+	int num_4 = (number % 100) % 10;
+	int mltpctn = num_1 * num_2 * num_3 * num_4;
+	cout << number << endl << num_1 << endl << num_2 << endl << num_3 << endl << num_4 << endl << endl;
+	if ((mltpctn % 4) == 0 && mltpctn != 0) {
+		cout << "Число " << mltpctn << " делится на 4 без остатка." << endl;
 	}
-	else {
-		if (proizv % 4 == 0) cout << "Число " << number << " кратно произведению его цифр" << endl;
-		else cout << "Число " << number << " не кратно произведению его цифр" << endl;
-	}
-	
+	else if (mltpctn == 0) cout << "Число равно 0, поэтому не может быть разделено." << endl;
+	else cout << "Число " << mltpctn << " не делится на 4 без остатка." << endl;
 	system("pause");
 	return 0;
 }
