6a31e318d72016186fec272edd028c98b667186d
Роман <djurball@list.ru>
2018-05-08 22:00:29 +0800

    Add files via upload

---
+++ 11.40.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include <iostream>
+#include <ctime>
+#define n 10
+using namespace std;
+
+/*
+	Задание 11.37
+*/
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand((unsigned)time(NULL));
+	int arr[n];
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 10 + 50;
+		cout << " [" << arr[i] << "] ";
+	}
+	
+	for (int i = 0; i < n; i++) {
+		if (arr[i] >= 0)
+			cout << " [" << arr[i] << "] ";
+		else cout << " [" << arr[i] << "] ";
+	}
+
+	system("pause");
+	return 0;
+}
